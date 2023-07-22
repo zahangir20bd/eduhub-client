e71f2d2cd02cd76ae35e5fc91ec37a63de7d0fe3
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-07-22 23:13:44 +0600

    Create a project for edu-hub-client

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+  return (
+    <div>
+      <h2>Nav bar</h2>
+    </div>
+  );
+};
+
+export default NavBar;
